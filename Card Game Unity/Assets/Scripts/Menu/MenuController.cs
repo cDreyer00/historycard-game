c472ee0e7ed62e4d821b4f1e127203c3262cf6db
CDreyer <cristianbr.dreyer@gmail.com>
2021-07-20 04:05:52 -0300

    script of menu changed

---
+++ Card Game Unity/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    string button;
+
+    private void Start()
+    {
+
+    }
+
+    public void MenuTransition(string button)
+    {
+        if(button == "Play")
+        {
+            Debug.Log("jogar");
+        }
+        else if (button == "Options")
+        {
+            Debug.Log("opções");
+
+        }
+        else if (button == "Quit")
+        {
+            Debug.Log("Sair");
+        }
+    }
+}

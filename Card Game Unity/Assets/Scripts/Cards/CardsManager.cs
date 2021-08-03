7b6840381eba4529965c428751e794b948702180
CDreyer <cristianbr.dreyer@gmail.com>
2021-08-03 00:58:33 -0300

    Gameplay changes

---
+++ Card Game Unity/Assets/Scripts/Cards/CardsManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsManager : MonoBehaviour
+{
+    public Sprite[] cards;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject Card = new GameObject("card from" + gameObject.name, typeof(SpriteRenderer), typeof(BoxCollider));
+        Card.transform.position = transform.position - new Vector3(0, 3, 0);
+        Card.GetComponent<SpriteRenderer>().sprite = cards[Random.Range(0, cards.Length)];
+    }
+}

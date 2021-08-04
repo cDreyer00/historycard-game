6d457182bc4d2f5eccd0957cedc33eb3ef0f2e61
CDreyer <cristianbr.dreyer@gmail.com>
2021-08-04 01:18:14 -0300

    gameplay canvas

--- Card Game Unity/Assets/Scripts/Cards/CardsManager.cs
+++ Card Game Unity/Assets/Scripts/Cards/CardsManager.cs
@@ -23,12 +23,13 @@ public class CardsManager : MonoBehaviour
         if (CanInteract)
         {
             // cria uma carta aleatória dentre as opções
-            GameObject Card = new GameObject("card from" + gameObject.name, typeof(SpriteRenderer), typeof(BoxCollider));
+            GameObject Card = new GameObject("Card from " + gameObject.name, typeof(SpriteRenderer), typeof(BoxCollider));
             Card.transform.position = transform.position - new Vector3(0, 2, 0);
             Card.GetComponent<SpriteRenderer>().sprite = cards[Random.Range(0, cards.Length)];
 
             transform.position = transform.position + new Vector3(0, 2, 0); // sobe o deck de cartas ja retirada
-            CanInteract = false;
+
+            CanInteract = false; // impede de interagir novamente
         }
 
 

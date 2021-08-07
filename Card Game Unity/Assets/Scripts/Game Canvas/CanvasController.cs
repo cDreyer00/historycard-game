80c5109f30e2d69fd2dfa5c16a5fab36c2bf3987
CDreyer <cristianbr.dreyer@gmail.com>
2021-08-07 04:40:59 -0300

    player prefs att

--- Card Game Unity/Assets/Scripts/Game Canvas/CanvasController.cs
+++ Card Game Unity/Assets/Scripts/Game Canvas/CanvasController.cs
@@ -7,12 +7,16 @@ using UnityEngine;
 public class CanvasController : MonoBehaviour
 {
     public TextMeshProUGUI roomCode;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI playerNameText;
+
 
     private void Start()
     {
         roomCode.text = Connection.RoomCode; // mostra o código da sala no canvas
 
-        Debug.Log("yout name is: " + PlayerPrefs.GetString("Nick"));
+        Debug.Log("your name is: " + PlayerPrefs.GetString("Nick"));
+        playerNameText.text = PlayerPrefs.GetString("Nick");
 
     }
 }

572534e446b34f8fe5672847752b1c99154a72d4
CDreyer <cristianbr.dreyer@gmail.com>
2021-08-10 16:07:51 -0300

    PUN nickname

--- Card Game Unity/Assets/Scripts/Game Canvas/CanvasController.cs
+++ Card Game Unity/Assets/Scripts/Game Canvas/CanvasController.cs
@@ -1,10 +1,11 @@
-﻿using Photon.Realtime;
+﻿using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class CanvasController : MonoBehaviour
+public class CanvasController : MonoBehaviourPunCallbacks
 {
     public TextMeshProUGUI roomCode;
     public TextMeshProUGUI timerText;
@@ -15,8 +16,18 @@ public class CanvasController : MonoBehaviour
     {
         roomCode.text = Connection.RoomCode; // mostra o código da sala no canvas
 
-        Debug.Log("your name is: " + PlayerPrefs.GetString("Nick"));
-        playerNameText.text = PlayerPrefs.GetString("Nick");
+        photonView.RPC("NewPlayer", RpcTarget.AllBuffered, PlayerPrefs.GetString("Nick"));
+        //NewPlayer(PlayerPrefs.GetString("Nick"));
+    }
+
+    [PunRPC]
+    public void NewPlayer(string Nick)
+    {
+        GameObject newText = Instantiate(playerNameText.gameObject, transform.position, transform.rotation);
+
+        newText.transform.parent = playerNameText.transform.parent;
+
+        newText.GetComponent<TextMeshProUGUI>().text = Nick;
 
     }
 }
